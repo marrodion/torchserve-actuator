61afd45435e9ad79fdac03d253969db800de94e4
Rodion Martynov <mar.rodion@gmail.com>
2023-06-21 10:37:46 +0100

    Info endpoint GET impl.

--- torchserve-actuator/src/main/java/org/pytorch/serve/plugins/endpoint/actuator/info/InfoEndpoint.java
+++ torchserve-actuator/src/main/java/org/pytorch/serve/plugins/endpoint/actuator/info/InfoEndpoint.java
@@ -1,16 +1,32 @@
 package org.pytorch.serve.plugins.endpoint.actuator.info;
 
+import static java.net.HttpURLConnection.HTTP_OK;
+
+import com.google.gson.GsonBuilder;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.time.format.DateTimeFormatter;
 import org.pytorch.serve.servingsdk.Context;
 import org.pytorch.serve.servingsdk.ModelServerEndpoint;
 import org.pytorch.serve.servingsdk.ModelServerEndpointException;
 import org.pytorch.serve.servingsdk.http.Request;
 import org.pytorch.serve.servingsdk.http.Response;
 
-import java.io.IOException;
-
 public class InfoEndpoint extends ModelServerEndpoint {
-    @Override
-    public void doGet(Request req, Response res, Context ctx) throws ModelServerEndpointException, IOException {
-        super.doGet(req, res, ctx);
-    }
+  private static final InfoResponse INFO_RESPONSE = InfoImplementation.getInfo();
+  private static final String JSON_HEADER = "application/json";
+
+  @Override
+  public void doGet(Request req, Response res, Context ctx)
+      throws ModelServerEndpointException, IOException {
+    res.setStatus(HTTP_OK);
+    res.setContentType(JSON_HEADER);
+    res.getOutputStream()
+        .write(
+            new GsonBuilder()
+                .setDateFormat(DateTimeFormatter.ISO_OFFSET_DATE_TIME.toString())
+                .create()
+                .toJson(INFO_RESPONSE)
+                .getBytes(StandardCharsets.UTF_8));
+  }
 }

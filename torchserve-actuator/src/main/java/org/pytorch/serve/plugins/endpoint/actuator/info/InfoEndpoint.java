1cb5797712e32ca95237fedde0631b9c5f15ca99
Rodion Martynov <mar.rodion@gmail.com>
2023-06-21 16:24:02 +0100

    Use json instead of converting on the fly

--- torchserve-actuator/src/main/java/org/pytorch/serve/plugins/endpoint/actuator/info/InfoEndpoint.java
+++ torchserve-actuator/src/main/java/org/pytorch/serve/plugins/endpoint/actuator/info/InfoEndpoint.java
@@ -2,10 +2,8 @@ package org.pytorch.serve.plugins.endpoint.actuator.info;
 
 import static java.net.HttpURLConnection.HTTP_OK;
 
-import com.google.gson.GsonBuilder;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.time.format.DateTimeFormatter;
 import org.pytorch.serve.servingsdk.Context;
 import org.pytorch.serve.servingsdk.ModelServerEndpoint;
 import org.pytorch.serve.servingsdk.ModelServerEndpointException;
@@ -13,7 +11,7 @@ import org.pytorch.serve.servingsdk.http.Request;
 import org.pytorch.serve.servingsdk.http.Response;
 
 public class InfoEndpoint extends ModelServerEndpoint {
-  private static final InfoResponse INFO_RESPONSE = InfoImplementation.getInfo();
+  private static final String INFO_RESPONSE = InfoImplementation.getInfoJson();
   private static final String JSON_HEADER = "application/json";
 
   @Override
@@ -21,12 +19,6 @@ public class InfoEndpoint extends ModelServerEndpoint {
       throws ModelServerEndpointException, IOException {
     res.setStatus(HTTP_OK);
     res.setContentType(JSON_HEADER);
-    res.getOutputStream()
-        .write(
-            new GsonBuilder()
-                .setDateFormat(DateTimeFormatter.ISO_OFFSET_DATE_TIME.toString())
-                .create()
-                .toJson(INFO_RESPONSE)
-                .getBytes(StandardCharsets.UTF_8));
+    res.getOutputStream().write(INFO_RESPONSE.getBytes(StandardCharsets.UTF_8));
   }
 }

f527c0a109a14f1cbe51306f26c5edcdd5fe6ee7
Rodion Martynov <mar.rodion@gmail.com>
2023-07-12 16:39:56 +0100

    Use PathSegments parser in actuator endpoint

--- torchserve-actuator/src/main/java/org/pytorch/serve/plugins/endpoint/actuator/ActuatorRestEndpoint.java
+++ torchserve-actuator/src/main/java/org/pytorch/serve/plugins/endpoint/actuator/ActuatorRestEndpoint.java
@@ -1,8 +1,5 @@
 package org.pytorch.serve.plugins.endpoint.actuator;
 
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.util.Map;
 import org.pytorch.serve.plugins.endpoint.actuator.health.HealthEndpoint;
 import org.pytorch.serve.plugins.endpoint.actuator.info.InfoEndpoint;
 import org.pytorch.serve.servingsdk.Context;
@@ -12,24 +9,32 @@ import org.pytorch.serve.servingsdk.annotations.helpers.EndpointTypes;
 import org.pytorch.serve.servingsdk.http.Request;
 import org.pytorch.serve.servingsdk.http.Response;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+
 @Endpoint(
     urlPattern = "actuator",
     endpointType = EndpointTypes.INFERENCE,
     description = "Actuator health status")
 public class ActuatorRestEndpoint extends ModelServerEndpoint {
-  // TODO: Refactor to a proper routing based on URI path parsing
+  private static final String PREFIX = "actuator";
   private static final Map<String, ModelServerEndpoint> endpoints =
       Map.of("health", new HealthEndpoint(), "info", new InfoEndpoint());
 
   @Override
   public void doGet(Request req, Response rsp, Context ctx) throws IOException {
-    switch (req.getRequestURI()) {
-      case "/actuator" -> doGetActuator(rsp);
-      case "/actuator/health" -> getEndpointByName("health").doGet(req, rsp, ctx);
-      case "/actuator/info" -> getEndpointByName("info").doGet(req, rsp, ctx);
-      default -> {
+    PathSegments segments = new PathSegments(req.getRequestURI());
+    if (PREFIX.equals(segments.getPrefix())) {
+      if ("".equals(segments.getEndpointName())) {
+        doGetActuator(rsp);
+      } else if (endpoints.containsKey(segments.getEndpointName())) {
+        endpoints.get(segments.getEndpointName()).doGet(req, rsp, ctx);
+      } else {
         notFoundResponse(req, rsp);
       }
+    } else {
+      notFoundResponse(req, rsp);
     }
   }
 
@@ -44,9 +49,5 @@ public class ActuatorRestEndpoint extends ModelServerEndpoint {
         .write(
             String.format("%s Not found", req.getRequestURI())
                 .getBytes(StandardCharsets.UTF_8));
-  }
-
-  private ModelServerEndpoint getEndpointByName(String name) {
-    return endpoints.get(name);
   }
 }

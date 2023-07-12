2bd00041b0b43e9f53c22d3e118020727f7dd108
Rodion Martynov <mar.rodion@gmail.com>
2023-07-12 18:13:37 +0100

    Fix PMD violations

--- torchserve-actuator/src/main/java/org/pytorch/serve/plugins/endpoint/actuator/NotFoundResponse.java
+++ torchserve-actuator/src/main/java/org/pytorch/serve/plugins/endpoint/actuator/NotFoundResponse.java
@@ -5,8 +5,10 @@ import java.nio.charset.StandardCharsets;
 import org.pytorch.serve.servingsdk.http.Request;
 import org.pytorch.serve.servingsdk.http.Response;
 
-public class NotFoundResponse {
+public final class NotFoundResponse {
   private static final int HTTP_NOT_FOUND = 404;
+
+  private NotFoundResponse() {}
 
   public static void notFoundResponse(Request req, Response rsp) throws IOException {
     rsp.setStatus(HTTP_NOT_FOUND);
--- torchserve-actuator/src/main/java/org/pytorch/serve/plugins/endpoint/actuator/health/HealthResponse.java
+++ torchserve-actuator/src/main/java/org/pytorch/serve/plugins/endpoint/actuator/health/HealthResponse.java
@@ -1,6 +1,6 @@
 package org.pytorch.serve.plugins.endpoint.actuator.health;
 
-public abstract class HealthResponse {
+public class HealthResponse {
   protected HealthStatus status;
 
   public HealthStatus getStatus() {
